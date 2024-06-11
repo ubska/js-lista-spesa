062aa90fd3a12c36650249e2be9ab4223000b289
Oleksandra Ubska <alessandraubska@gmail.com>
2024-06-11 16:30:22 +0200

    fix list

--- js/main.js
+++ js/main.js
@@ -3,4 +3,21 @@ Data una lista della spesa, stampare sulla pagina(anche brutalmente, basta che s
 */
 
 // creo un array con la lista della spesa
-const listaDellaSpesa = ["Pane","Latte","Uova","Pasta","Pomodori","Formaggio","Mela",];
+const listaDellaSpesa = ["Pane", "Latte", "Uova", "Pasta", "Pomodori", "Formaggio", "Mela",];
+console.log(listaDellaSpesa);
+
+// seleziono ul dove aggiungero succesivamente gli elementi (li) della spesa
+const ulLista = document.getElementById("lista_spesa");
+console.log(ulLista);
+
+// inizializzo il contatore/indice (i)
+let i = 0;
+// continuo il cilco fino a quando (i) è minore della lunghezza della lista
+while (i < listaDellaSpesa.length) {
+    // creo elemento (li)
+    let liLista = document.createElement("li");
+    liLista.textContent = listaDellaSpesa[i];
+    ulLista.appendChild(liLista);
+    i++;
+}
+    
