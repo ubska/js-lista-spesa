4fdec45d20d431fff7f4f21a6d395553e4f88af1
Oleksandra Ubska <alessandraubska@gmail.com>
2024-06-11 14:29:27 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/*
+Data una lista della spesa, stampare sulla pagina(anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+*/
+
+// creo un array con la lista della spesa
+const listaDellaSpesa = ["Pane","Latte","Uova","Pasta","Pomodori","Formaggio","Mela",];
